02387d9fbd657ad5dd8b573c67a8d040a2cc6b11
youngwise <sergeylazarev2004@gmail.com>
2024-09-21 18:20:30 +0300

    Инициализция проекта

---
+++ src/main/java/com/mtuci/lazarev/LazarevApplication.java
@@ -0,0 +1,13 @@
+package com.mtuci.lazarev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazarevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazarevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtuci/lazarev/LazarevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtuci.lazarev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazarevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
